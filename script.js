596d56c33237ee0a131b2f839ce75a5c9e6608de
HitTheBrick <82474223+HitTheBrick@users.noreply.github.com>
2021-06-16 13:45:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -276,44 +276,9 @@ To make a new level, copy the following format into a new line on the levels var
 	
 */
 
-function touchHandler(event) {
-  var touches = event.changedTouches,
-    first = touches[0],
-    type = "";
-  switch (event.type) {
-    case "touchstart":
-      type = "mousedown";
-      break;
-    case "touchmove":
-      type = "mousemove";
-      break;
-    case "touchend":
-      type = "mouseup";
-      break;
-    default:
-      return;
-  }
 
-  // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
-  //                screenX, screenY, clientX, clientY, ctrlKey, 
-  //                altKey, shiftKey, metaKey, button, relatedTarget);
 
-  var simulatedEvent = document.createEvent("MouseEvent");
-  simulatedEvent.initMouseEvent(type, true, true, window, 1,
-    first.screenX, first.screenY,
-    first.clientX, first.clientY, false,
-    false, false, false, 0 /*left*/ , null);
 
-  first.target.dispatchEvent(simulatedEvent);
-  event.preventDefault();
-}
-
-function init() {
-  document.addEventListener("touchstart", touchHandler, true);
-  document.addEventListener("touchmove", touchHandler, true);
-  document.addEventListener("touchend", touchHandler, true);
-  document.addEventListener("touchcancel", touchHandler, true);
-}
 
 function ImageCollection(list, callback) {
   var total = 0,
@@ -413,6 +378,7 @@ canvas.addEventListener("mousedown", function(e) {
     isClicked = true;
   }
 });
+
 window.addEventListener("mouseup", function(e) {
   getMousePosition(canvas, e);
   isClicked = false;
